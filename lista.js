345ff21e6e0bcf34682de83d584c29b42acf9d80
Adhemar Jr <adhemar.machado@gmail.com>
2021-10-13 20:18:37 -0300

    Second condition using first if()

--- lista.js
+++ lista.js
@@ -1,6 +1,10 @@
-let numeroAlunos = 20;
+let numeroAlunos = 5;
 
 for (let index = 0; index < numeroAlunos; index++) {
     console.log(index);
     
+    if (index == 0) {
+        console.log("ZERO");
+        
+    }
 }
