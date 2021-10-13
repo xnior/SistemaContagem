6371f3dabf54313430273f16e829ddf670032e76
Adhemar Jr <adhemar.machado@gmail.com>
2021-10-13 20:45:55 -0300

    Inserindo no sistema Interpolação

--- lista.js
+++ lista.js
@@ -4,11 +4,11 @@ for (let index = 0; index < numeroAlunos; index++) {
     // console.log(index);
     
     if (index == 0) {
-        console.log(index + " ZERO");
+        console.log(index + ": ZERO"); // Concatenação
     } else if (index%2 == 0) {
-        console.log(index + " PAR");
+        console.log(`${index} PAR`); //Interpolação
     }else{
-        console.log(index + " IMPAR");
+        console.log(`${index} IMPAR`); //Interpolação
     }
 
 }
