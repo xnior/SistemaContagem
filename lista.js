50461345e107e97e767b1e6fc0ed7ad0ad480fde
Adhemar Jr <adhemar.machado@gmail.com>
2021-10-13 21:15:11 -0300

    Adicionado a função While

--- lista.js
+++ lista.js
@@ -1,14 +1,33 @@
 let numeroAlunos = 5;
 
-for (let index = 0; index < numeroAlunos; index++) {
-    // console.log(index);
-    
-    if (index == 0) {
-        console.log(index + ": ZERO"); // Concatenação
-    } else if (index%2 == 0) {
-        console.log(`${index}: PAR`); //Interpolação
+let listaAlunos = ["José", "Maria", "Antônio", "Marcos", "Joaquim", "Jonas"]
+
+let contador = 0;
+
+while (contador < listaAlunos.length) {
+   
+    if (contador == 0) {
+        console.log(contador + ": ZERO"); // Concatenação
+    } else if (contador%2 == 0) {
+        console.log(`${contador}: PAR`); //Interpolação
     }else{
-        console.log(`${index}: IMPAR`); //Interpolação
+        console.log(`${contador}: IMPAR`); //Interpolação
     }
 
+    contador++;
+
 }
+
+// for (let index = 0; index < numeroAlunos; index++) {
+//     // console.log(index);
+
+    // if (index == 0) {
+    //     console.log(index + ": ZERO"); // Concatenação
+    // } else if (index%2 == 0) {
+    //     console.log(`${index}: PAR`); //Interpolação
+    // }else{
+    //     console.log(`${index}: IMPAR`); //Interpolação
+    // }
+
+// }
+
